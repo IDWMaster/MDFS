5f420e75e385b24e64ed6e9df63d637b2039b4fb
Brian Bosak <webadm@elcnet.servehttp.com>
2015-07-26 21:58:52 -0500

    Added hashing specification

--- fsmod.h
+++ fsmod.h
@@ -1,6 +1,16 @@
 #include <stddef.h>
-
+#include <stdint.h>
+typedef struct {
+  uint64_t block[2];
+  uint64_t offset;
+  uint64_t count;
+} WORK_ORDER;
 
 extern "C" {
-  void FetchRequest();
+  //Requests a block work request from the filesystem driver
+  void GetWork(WORK_ORDER* output);
+  //Marks a work request as completed and verified. Each plugin is responsible for verifying the integrity of each completed work item.
+  //Plugins that work over remote networks should take special care to cryptographically protect data; using mechanisms such as SHA
+  //hashing and cryptographic signatures, if applicable. Each agent should ensure that all blocks are also the most recent version.
+  void CompleteWork(WORK_ORDER* order, const void* data, uint64_t len);
 }
--- main.cpp
+++ main.cpp
@@ -24,6 +24,10 @@ public:
   GUID(const unsigned char* val) {
     memcpy(this->val,val,16);
   }
+  GUID(const uint64_t* val) {
+    this->val[0] = val[0];
+    this->val[1] = val[1];
+  }
   bool operator<(const GUID& other) const {
     return uuid_compare((unsigned char*)val,(unsigned char*)other.val)<0;
   }
@@ -64,9 +68,54 @@ static GUID GLOBAL_ROOT;
 
 
 
+class BlockRequest {
+public:
+  GUID block;
+  uint64_t offset;
+  uint64_t len;
+  BlockRequest() {
+  }
+  bool operator<(const BlockRequest& other) const {
+    return memcmp(&other,this,sizeof(this)) < 0;
+  }
+  bool operator==(const BlockRequest& other) const {
+    return memcmp(&other,this,sizeof(this)) == 0;
+  }
+};
+
 //Pending request list
 
-static std::map<GUID,GGClient::WaitHandle> pendingRequests;
+static std::map<BlockRequest,GGClient::WaitHandle*> pendingRequests;
+static GGClient::WaitHandle workEvent;
+
+
+static GGClient::WaitHandle* RequestBlockSegment(const BlockRequest& req) {
+  mtx.lock();
+  auto bot = new GGClient::WaitHandle();;
+  pendingRequests[req] = bot;
+  workEvent.Signal();
+  mtx.unlock();
+  return bot;
+}
+
+
+
+static bool RetrieveBlock(const GUID& block, uint64_t position, uint64_t len, void* output) {
+  BlockRequest req;
+  req.block = block;
+  req.offset = position;
+  req.len = len;
+  GGClient::WaitHandle* wh = RequestBlockSegment(req);
+  wh->Fetch();
+  if(wh->data) {
+    memcpy(output,wh->data,len);
+  }
+  wh->Unfetch();
+  return wh->data;
+  
+}
+
+
 
 
 
@@ -75,10 +124,35 @@ static std::map<GUID,GGClient::WaitHandle> pendingRequests;
 extern "C" {
   
   
-  
-  void REG_Base_Plugins() {
-    
+  void GetWork(WORK_ORDER* output) {
+    bool found = false;
+    BlockRequest request;
+    mtx.lock();
+    if(!pendingRequests.empty()) {
+      found = true;
+      request = pendingRequests.begin()->first;
+    }
+    mtx.unlock();
+    if(found) {
+      memcpy(output->block,request.block.val,16);
+      output->offset = request.offset;
+      output->count = request.len;
+    }
   }
+  void CompleteWork(WORK_ORDER* order, const void* data, uint64_t len) {
+    BlockRequest request;
+    memcpy(request.block.val,data,16);
+    request.offset = order->offset;
+    request.len = order->count;
+    mtx.lock();
+    auto bot = pendingRequests.find(request);
+    if(bot != pendingRequests.end()) {
+      //Signal WORK_ORDER
+      bot->second->Put((unsigned char*)data,len);
+    }
+    mtx.unlock();
+  }
+  
   
   
 }
@@ -153,23 +227,22 @@ static void AES_Decrypt(const unsigned char* key, uint64_t* id, uint64_t blockse
 
 
 
+//TODO: Hash data into blocks of 4KB using AES-CBC as hashing algorithm to prevent manipulation of data
+//Last 4KB of data in each chunk will be all zeroes. If the data is NOT all zeroes; it indicates the block is corrupt.
+
 
 class EncryptedStream {
 public:
+  //Base GUID
   GUID baseIndex;
   unsigned char key[32];
   EncryptedStream(const GUID& baseGuid, const unsigned char* key) {
     baseIndex = baseGuid;
-    //NOTE: Allocation block size is determined by block header. Block size must be divisible by 16.
-    /*HEADER STRUCTURE
-     * Block size (uint32_t)
-     * Number of blocks in list (uint64_t)
-     * User attributes (uint32_t)
-     * Block list
-     * */
-    uint64_t data[2];
+    memcpy(this->key,key,32);
     
   }
+  
+  
   
 };
 
@@ -202,7 +275,7 @@ static int Main(int argc, char** argv) {
   localstordir = "blocks/";
 fuse_operations opt;
 memset(&opt,0,sizeof(opt));
-struct fuse_operations op;
+opt.getdir = EnumerateDirectory;
 int fd;
 if((fd = open("config",O_RDONLY)) == -1) {
   //Create config file
@@ -219,7 +292,7 @@ fd = open("config",O_RDONLY);
 }
 read(fd,GLOBAL_ROOT.val,16);
 read(fd,GLOBAL_KEY,32);
-fuse_main(argc,argv,&op);
+fuse_main(argc,argv,&opt);
 
 return 0;
 
